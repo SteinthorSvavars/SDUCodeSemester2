46665138b52ed6c460b46d3985b51bc15830ff2d
Steinthor Emil Svavarsson <steinthor.emil.svavarsson@gmail.com>
2025-06-01 18:34:42 +0200

    Week6Works

--- Sem2/Week6/src/main.c
+++ Sem2/Week6/src/main.c
@@ -5,30 +5,68 @@
  * Author : Alin
  */ 
 
-
-
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <avr/io.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
 
+/* I2C coms */
+#include "i2cmaster.h"
+/* LCD */ 
+#include "lcd.h"
+/* Temp sens */
+#include "lm75.h"
+
+//#include "twimaster.c"
 #include "usart.h"
 
-int main(void) {  
+unsigned int addr1 = 0;
+unsigned int addr2 = 4;
 
-  int input;  
+int main() {  
 
-  uart_init(); // open the communication to the microcontroller
-  io_redirect(); // redirect input and output to the communication
+  i2c_init();
+  LCD_init();
+  lm75_init();
 
-    
-  while(1) {
-		
-	  printf("Type in a number \n");
-    scanf("%d", &input);
-    printf("The number you typed is %d is %x in hexadecimal \n", input, input);
-	  _delay_ms(1000)	;
+  float current_temp;
+  float min_temp;
+  float max_temp;
 
+  /*Buttor Config*/
+  DDRC = 0xF0;
+  PORTC = 0x3F;
+
+  min_temp = eeprom_read_float((uint8_t*)addr1);
+  max_temp = eeprom_read_float((uint8_t*)addr2);
+
+  while (1){
+    current_temp = (float)get_temperature();
+    if(current_temp < min_temp){
+      min_temp = current_temp;
+      eeprom_write_float((uint8_t*)addr1, (float)min_temp);
+    }
+    if(current_temp > max_temp){
+      max_temp = current_temp;
+      eeprom_write_float((uint8_t*)addr2, (float)max_temp);
+    }
+
+    LCD_set_cursor(0,0);
+    printf("CurTempIs %.2f", current_temp);
+    LCD_set_cursor(0,1);
+    printf("MinTempIs %.2f", min_temp);
+    LCD_set_cursor(0,2);
+    printf("MaxTempIs %.2f", max_temp);
+
+    _delay_ms(1000);
+
+    if(PINC == 0x37){
+      min_temp = current_temp;
+      max_temp = current_temp;
+      eeprom_write_float((uint8_t*)addr1, (float)min_temp);
+      eeprom_write_float((uint8_t)addr2, (float)max_temp);
+    }
   }
-  
-  return 0;
 }
